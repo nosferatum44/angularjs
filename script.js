190b1466df91d8c23b354e021f93d3c9934fd441
nosferatum44 <nosferatum44@gmail.com>
2019-10-17 12:29:26 +0300

    asd

--- script.js
+++ script.js
@@ -3,7 +3,27 @@
     url: 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=groQeNemKAhk7QjDWircgauo5jYVcwez'
 });*/
 
-var app = angular.module('app', []);
+var app = angular
+
+    .module("Demo", ["ui.router"])
+
+    .config(function ($stateProvider) {
+        
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: "home.html",
+                controller: "homeController",
+                
+            })
+            
+
+
+    })
+    .controller("homeController", function ($scope) {
+        $scope.message = "Home page";
+    })
+
 
 /* app.controller('firstCtrl', function($scope, myFactory) {
     console.log('firstCtrl');
@@ -46,18 +66,39 @@ app.factory('myFactory', function() {
     }
 });
 */
+// ----------------------------
 
-app.controller('mainCtrl', function ($http, $scope) {  
-    $http(
-        {
-           method: 'GET',
-           url: 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',  /*Most populars*/
-         /*  url: 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=groQeNemKAhk7QjDWircgauo5jYVcwez', /* Most recent */ 
-        }).then(function successCallback(result) {
-           console.log('success', result);
-           $scope.results = result.data.results;
+// app.controller('mainCtrl', function ($http, $scope) {
+//     $http(
+//         {
+//             method: 'GET',
+//             url: 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',  /*Most populars*/
+//             /*  url: 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=groQeNemKAhk7QjDWircgauo5jYVcwez', /* Most recent */
+//         }).then(function successCallback(result) {
+//             console.log('success', result);
+//             $scope.results = result.data.results;
 
-        }, function errorCallback(result) {
-           console.log("there's a fucking error, man...");
-    });
-});
+//         }, function errorCallback(result) {
+//             console.log("there's a fucking error, man...");
+//         });
+// });
+
+// var i = 1;
+
+// function theme() {
+
+
+//     if (i == 1) {
+//         document.body.style.backgroundColor = "white";
+//         document.body.style.color = "black";
+//         document.getElementById("theme").value = "Night";
+//         i = 2;
+//     } else {
+//         document.body.style.backgroundColor = "#282c2b";
+//         document.body.style.color = "white";
+//         document.getElementById("theme").value = "Day";
+//         i = 1;
+//     }
+
+
+// };
