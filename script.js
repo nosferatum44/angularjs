0a03225649be6935f5be0948492fe8a3984c8478
nosferatum44 <nosferatum44@gmail.com>
2019-10-24 14:34:40 +0300

    asd

--- script.js
+++ script.js
@@ -1,243 +1,245 @@
 var app = angular
+	.module("noTrumpNews", ["ui.router", "slugifier", "ngSanitize"])
 
-    .module("Demo", ["ui.router", "slugifier", "ngSanitize"])
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+		$urlRouterProvider.otherwise('/')
 
-    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
-        $urlRouterProvider.otherwise('');
+		// Remove #! from url
+		$locationProvider.html5Mode({ enabled: true })
 
-        $stateProvider
-            .state("home", {
-                url: "/:page",
-                templateUrl: "templates/home.html",
-                controller: "homeController",
-                resolve: {
-                    articles: function ($stateParams, $http, $routeParams) {
-                        console.log('routeParams' + $routeParams)
-                        return $http(
-                            {
-                                method: 'GET',
-                                url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + '{{$stateParams.page || 1}}' + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',
-                            })
-                            
-                    }
-                },
-                params: {
-                    page: '1'
-                }
-               
-            })
+		$stateProvider
+			.state('app', {
+				url: "?page",
+				resolve: {
+					article: angular.noop, //empty function
+					articles: angular.noop
+				},
+				params: {
+					page: null,
+					articleSlug: null
+				}
+			})
 
-            .state('article', {
-                url: "/article/:articleSlug",
-                templateUrl: "templates/article.html",
-                controller: "articleController",
-                resolve: {
-                    article: function ($stateParams, $http) {
-                        var formData = new FormData();
+			.state('app.home', {
+				url: "/",
+				templateUrl: "templates/home.html",
+				controller: "homeController",
+				resolve: {
+					articles: function ($stateParams, $http) {
+						console.log($stateParams)
+						return $http(
+							{
+								method: 'GET',
+								url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page='
+									+ ($stateParams.page || 6)
+									+ '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez'
+							}
+						).then(function (result) {
+							window.setTimeout(function () {
+								$('.grid').isotope({
+									itemSelector: '.box',
+									horizontalOrder: true,
+									masonry: {
+										columnWidth: 50
+									}
+								});
+							}, 100)
+							return result
+						})
+					}
+				}
+			})
 
-                        formData.append("url", $stateParams.url);
+			.state('app.article', {
+				url: "/article/:articleSlug",
+				templateUrl: "templates/article.html",
+				controller: "articleController",
+				resolve: {
+					article: function ($stateParams, $http) {
+						var formData = new FormData()
+						formData.append("url", $stateParams.url)
+						formData.append("API_KEY", "46014746951CA2FDB1D88548963945FD")
 
-                        console.log($stateParams);
-                        formData.append("API_KEY", "46014746951CA2FDB1D88548963945FD");
-                        return $http(
-                            {
-                                method: 'POST',
-                                url: 'https://resoomer.pro/websummarizer/',
-                                data: formData,
-                                headers: { 'Content-Type': undefined },
-                            }
-                        )
-                    },
-                },
-                params: {
-                    url: null,
-                    image: '',
-                    title: '',
-                    externalUrl: ''
-                }
-            })
+						return $http(
+							{
+								method: 'POST',
+								url: 'https://resoomer.pro/websummarizer/',
+								data: formData,
+								headers: { 'Content-Type': undefined }
+							}
+						)
+					}
+				},
+				params: {
+					url: null,
+					image: '',
+					title: '',
+					externalUrl: '',
+					articleSlug: ''
+				}
+			})
+	}])
 
+	.controller("homeController", function (articles, $scope, $sanitize) {
+		$scope.articles = articles.data.response.docs
+		console.log(articles)
+	})
 
-        /* Remove #! from url */
-        // $locationProvider.html5Mode({
-        //     enabled: true,
-        //     requireBase: false
-        // });
+	.controller("articleController", function (article, $scope, $stateParams) {
+		window.scrollTo({ top: 0 })
+		console.log(article)
+		$scope.articleText = article.data.mediumText.content
+		$scope.image = $stateParams.image
+		$scope.title = $stateParams.title
+		$scope.externalUrl = $stateParams.externalUrl
+	})
 
-    }])
-    .controller("homeController", function () {
-        $articles = articles.data.response.docs
-        console.log($articles)
-    })
-    .controller("articleController", function (article, $scope, $stateParams) {
-        window.scrollTo({ top: 0 })
-        console.log(article)
-        $scope.articleText = article.data.mediumText.content
-        $scope.image = $stateParams.image
-        $scope.title = $stateParams.title
-        $scope.externalUrl = $stateParams.externalUrl
-    })
-
-
-
-    .controller('mainCtrl', function ($http, $scope) {
-        $scope.pageNumber = 9
-
-
-
-        // $http(
-        //     {
-        //         method: 'GET',
-        //         url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',
-        //     }).then(function successCallback(result) {
-        //         console.log('success1', result);
-        //         $scope.results = result.data.response.docs;
-        //         console.log($scope.results)
-
-        //         window.setTimeout(function () {
-        //             $('.grid').isotope({
-        //                 itemSelector: '.box',
-        //                 horizontalOrder: true,
-        //                 masonry: {
-        //                     columnWidth: 50
-        //                 }
-        //             });
-        //         }, 1000)
-
-        //         $scope.divideInColumns = function () {
-        //         $scope.resultsTwoColumnsA = $scope.results.slice(0, 5)
-        //             $scope.resultsTwoColumnsB = $scope.results.slice(5)
-
-        //             $scope.resultsThreeColumnsA = $scope.results.slice(0, 3)
-        //             $scope.resultsThreeColumnsB = $scope.results.slice(3, 7)
-        //             $scope.resultsThreeColumnsC = $scope.results.slice(7)
-
-        //             window.scrollTo({ top: 0 })
-        //         }
-        //         $scope.divideInColumns()
-
-        //     }, function errorCallback(result) {
-        //         console.log("there's a fucking error, man...");
-        //     });
+	.controller('mainCtrl', function ($http, $scope) {
+		$scope.pageNumber = 9
 
 
 
-        $scope.mainPage = function () {
+		// $http(
+		//     {
+		//         method: 'GET',
+		//         url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',
+		//     }).then(function successCallback(result) {
+		//         console.log('success1', result);
+		//         $scope.results = result.data.response.docs;
+		//         console.log($scope.results)
 
-            window.location.reload()
-
-        }
-
-        //Pagination 
-
-
-        $scope.nextPage = function () {
-
-
-
-            $scope.pageNumber++
-            $http(
-                {
-                    method: 'GET',
-                    url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',
-                }).then(function successCallback(result) {
-                    console.log('success', result);
-                    $scope.results = result.data.response.docs;
-
-                    window.setTimeout(function () {
-                        $('.grid').isotope({
-                            itemSelector: '.box',
-                            horizontalOrder: true,
-                            masonry: {
-                                columnWidth: 50
-                            }
-                        });
-                    }, 1000)
-
-                    $scope.divideInColumns()
-
-                    document.querySelector('#previousPageNumber').value++
-                    document.getElementById('currentPageNumber').value++
-                    document.getElementById('nextPageNumber').value++
-                    document.querySelector('#previousPageNumber').style.display = "block"
-                    document.querySelector('#previousPage').style.display = "inline"
-
-                }, function errorCallback(result) {
-                    console.log("there's a fucking error, man...");
-                });
-        }
+		//         window.setTimeout(function () {
+		//             $('.grid').isotope({
+		//                 itemSelector: '.box',
+		//                 horizontalOrder: true,
+		//                 masonry: {
+		//                     columnWidth: 50
+		//                 }
+		//             });
+		//         }, 1000)
 
 
 
-        $scope.previousPage = function () {
-            if ($scope.pageNumber > 9) {
-                $scope.pageNumber--
-                $http(
-                    {
-                        method: 'GET',
-                        url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez', /* Most recent */
-                    }).then(function successCallback(result) {
-                        console.log('success', result);
-                        $scope.results = result.data.response.docs;
-
-                        window.setTimeout(function () {
-                            $('.grid').isotope({
-                                itemSelector: '.box',
-                                horizontalOrder: true,
-                                masonry: {
-                                    columnWidth: 50
-                                }
-                            });
-                        }, 1000)
-
-                        $scope.divideInColumns()
-
-                        document.getElementById('currentPageNumber').value--
-                        document.querySelector('#previousPageNumber').value--
-                        document.getElementById('nextPageNumber').value--
-                        if (document.querySelector('#currentPageNumber').value == 1) {
-                            document.querySelector('#previousPage').style.display = "none"
-                            document.querySelector('#previousPageNumber').style.display = "none"
-                        }
-
-                        window.scrollTo({ top: 0 })
-
-                    }, function errorCallback(result) {
-                        console.log("there's a fucking error, man...");
-                    });
-
-            }
-        }
-    }
+		//     }, function errorCallback(result) {
+		//         console.log("there's a fucking error, man...");
+		//     });
 
 
 
+		$scope.mainPage = function () {
 
-    );
+			window.location.reload()
+
+		}
+
+		//Pagination 
+
+
+		$scope.nextPage = function () {
+
+
+
+			$scope.pageNumber++
+			$http(
+				{
+					method: 'GET',
+					url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez',
+				}).then(function successCallback(result) {
+					console.log('success', result);
+					$scope.results = result.data.response.docs;
+
+					window.setTimeout(function () {
+						$('.grid').isotope({
+							itemSelector: '.box',
+							horizontalOrder: true,
+							masonry: {
+								columnWidth: 50
+							}
+						});
+					}, 1000)
+
+					window.scrollTo({ top: 0 })
+					document.querySelector('#previousPageNumber').value++
+					document.getElementById('currentPageNumber').value++
+					document.getElementById('nextPageNumber').value++
+					document.querySelector('#previousPageNumber').style.display = "block"
+					document.querySelector('#previousPage').style.display = "inline"
+
+				}, function errorCallback(result) {
+					console.log("there's a fucking error, man...");
+				});
+		}
+
+
+
+		$scope.previousPage = function () {
+			if ($scope.pageNumber > 9) {
+				$scope.pageNumber--
+				$http(
+					{
+						method: 'GET',
+						url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Politics")&fq=!trump&page=' + $scope.pageNumber + '&api-key=groQeNemKAhk7QjDWircgauo5jYVcwez', /* Most recent */
+					}).then(function successCallback(result) {
+						console.log('success', result);
+						$scope.results = result.data.response.docs;
+
+						window.setTimeout(function () {
+							$('.grid').isotope({
+								itemSelector: '.box',
+								horizontalOrder: true,
+								masonry: {
+									columnWidth: 50
+								}
+							});
+						}, 1000)
+
+						window.scrollTo({ top: 0 })
+						document.getElementById('currentPageNumber').value--
+						document.querySelector('#previousPageNumber').value--
+						document.getElementById('nextPageNumber').value--
+						if (document.querySelector('#currentPageNumber').value == 1) {
+							document.querySelector('#previousPage').style.display = "none"
+							document.querySelector('#previousPageNumber').style.display = "none"
+						}
+
+						window.scrollTo({ top: 0 })
+
+					}, function errorCallback(result) {
+						console.log("there's a fucking error, man...");
+					});
+
+			}
+		}
+	}
+
+
+
+
+	);
 
 
 /* Dark mode */
 var darkModeIndicator = 0;
 
 function theme() {
-    if (darkModeIndicator == 0) {
-        document.querySelector('body').classList.add("nightTheme");
-        document.querySelector('.svg').classList.add("nightTheme");
-        darkModeIndicator = 1;
-    } else {
-        document.querySelector("body").classList.remove("nightTheme");
-        document.querySelector(".svg").classList.remove("nightTheme");
-        darkModeIndicator = 0;
-    }
+	if (darkModeIndicator == 0) {
+		document.querySelector('body').classList.add("nightTheme");
+		document.querySelector('.svg').classList.add("nightTheme");
+		darkModeIndicator = 1;
+	} else {
+		document.querySelector("body").classList.remove("nightTheme");
+		document.querySelector(".svg").classList.remove("nightTheme");
+		darkModeIndicator = 0;
+	}
 };
 /* Dark mode END*/
 
 
 // slugify //
 function MyCtrl($scope, Slug) {
-    $scope.slugify = function (input) {
-        $scope.mySlug = Slug.slugify(input);
-    };
+	$scope.slugify = function (input) {
+		$scope.mySlug = Slug.slugify(input);
+	};
 }
 // slugify END//
 
